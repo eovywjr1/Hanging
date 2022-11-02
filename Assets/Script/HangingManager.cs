a521d7122c4539ea30da3c347cc57faa3a6fbd1f
eovywjr1 <40791869+eovywjr1@users.noreply.github.com>
2022-11-02 13:14:45 +0900

    timerSlide 구현

--- Assets/Script/HangingManager.cs
+++ Assets/Script/HangingManager.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HangingManager : MonoBehaviour
 {
     private bool isTimer = true;
-    private float workTime, workMaxTime;
+    private float workTime, workMaxTime = 10;
+
+    public Slider timerSlider;
+
+    private void Start()
+    {
+        workTime = workMaxTime;
+    }
 
     private void Update()
     {
         if (isTimer)
         {
             workTime -= Time.deltaTime;
+            timerSlider.value = workTime / workMaxTime;
             if(workTime <= 0)
             {
                 isTimer = false;
                 workTime = workMaxTime;
-                Debug.Log("time out");
             }
         }
     }
