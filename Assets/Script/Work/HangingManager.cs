f2938f8a9ed823dcdaa6cc154d40637fd6bf7f7a
eovywjr1 <40791869+eovywjr1@users.noreply.github.com>
2023-04-15 14:35:07 +0900

    충돌 해결

--- Assets/Script/Work/HangingManager.cs
+++ Assets/Script/Work/HangingManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Assets/Script/Work/HangingManager.cs
 using Kino;
 using System.Collections;
 using System.Collections.Generic;
@@ -58,10 +57,10 @@ public class HangingManager : MonoBehaviour, IListener
     {
         isTodesstrafe = true;
 
-        //»çÇü ÆÇº°//
+        //ï¿½ï¿½ï¿½ï¿½ ï¿½Çºï¿½//
         if (DistinguishTodesstrafe(0)) Debug.Log("True");
         else Debug.Log("False");
-        Debug.Log("»çÇü");
+        Debug.Log("ï¿½ï¿½ï¿½ï¿½");
 
         EndTodesstrafe();
     }
@@ -70,10 +69,10 @@ public class HangingManager : MonoBehaviour, IListener
     {
         isAmnesty = true;
 
-        //»çÇü ÆÇº°//
+        //ï¿½ï¿½ï¿½ï¿½ ï¿½Çºï¿½//
         if (DistinguishTodesstrafe(1)) Debug.Log("True");
         else Debug.Log("False");
-        Debug.Log("»ýÁ¸");
+        Debug.Log("ï¿½ï¿½ï¿½ï¿½");
 
         EndTodesstrafe();
     }
@@ -141,7 +140,7 @@ public class HangingManager : MonoBehaviour, IListener
         EventManager.instance.postNotification("badge", this, null);
     }
 
-    public IEnumerator StartStateWrong() //Áø¼ú¼­ Å¬¸¯ Æ²¸±¶§ 
+    public IEnumerator StartStateWrong() //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Å¬ï¿½ï¿½ Æ²ï¿½ï¿½ï¿½ï¿½ 
     {
         analogGlitch._isGlitch = true;
         yield return new WaitForSecondsRealtime(0.75f);
@@ -191,142 +190,4 @@ public class HangingManager : MonoBehaviour, IListener
                 break;
         }
     }
-=======
-using Kino;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class HangingManager : MonoBehaviour
-{
-    public bool isTodesstrafe;
-    public AttackerMouseMove attackerMouseMove;
-    public AttackerInfo attackerInfo;
-    public static int day =7, attackerCount = 1;
-    static bool isCorrect = true;
-    private AnalogGlitch analogGlitch;
-    [SerializeField] BossHand bossHand;
-    [SerializeField] GameObject convertEffect;
-
-    private void Awake()
-    {
-        attackerMouseMove = FindObjectOfType<AttackerMouseMove>();
-        attackerInfo = FindObjectOfType<AttackerInfo>();
-        analogGlitch = FindObjectOfType<AnalogGlitch>();
-    }
-
-    private void Start()
-    {
-        if (!isCorrect)
-        {
-            StartCoroutine(StartHoldOutHands());
-            isCorrect = true;
-        }
-    }
-
-    public void EndTodesstrafe()
-    {
-        attackerMouseMove.SetisPossibleTodesstrafe(false);
-        DestroyAllLineAndWindow();
-
-        isTodesstrafe = true;
-    }
-
-    public void Todesstrafe()
-    {
-        EndTodesstrafe();
-
-        //»çÇü ÆÇº°//
-        if (DistinguishTodesstrafe(0)) Debug.Log("True");
-        else Debug.Log("False");
-        Debug.Log("»çÇü");
-    }
-
-    public void UnTodesstrafe()
-    {
-        EndTodesstrafe();
-
-        //»çÇü ÆÇº°//
-        if (DistinguishTodesstrafe(1)) Debug.Log("True");
-        else Debug.Log("False");
-        Debug.Log("»ýÁ¸");
-    }
-
-    private bool DistinguishTodesstrafe(int mode)
-    {
-        if (mode == attackerInfo.recordData.isHanging)
-        {
-            isCorrect = false;
-            NextAttacker();
-            return true;
-        }
-        else
-        {
-            isCorrect = false;
-            StartCoroutine(StartGlitch());
-            return false;
-        }
-    }
-
-    private void DestroyAllLineAndWindow()
-    {
-        attackerMouseMove.StopAllCoroutines();
-        foreach (Line line in Line.lineList)
-        {
-            Destroy(line.lineObject);
-            Destroy(line.windowObject);
-        }
-        Line.lineList.Clear();
-    }
-
-    void NextAttacker()
-    {
-        attackerCount++;
-
-        StartCoroutine(ConvertSceneEffect());
-    }
-
-    IEnumerator ConvertSceneEffect()
-    {
-        convertEffect.SetActive(true);
-
-        yield return new WaitForSecondsRealtime(1.5f);
-
-        convertEffect.SetActive(false);
-        SceneManager.LoadScene("Merge");
-    }
-
-    IEnumerator StartGlitch()
-    {
-        analogGlitch._isGlitch = true;
-
-        yield return new WaitForSecondsRealtime(0.75f);
-
-        analogGlitch._isGlitch = false;
-        NextAttacker();
-    }
-
-
-    IEnumerator StartHoldOutHands()
-    {
-        yield return new WaitForSecondsRealtime(1.25f);
-
-        bossHand.holdOutHand();
-    }
-
-    public IEnumerator StartStateWrong() //Áø¼ú¼­ Å¬¸¯ Æ²¸±¶§ 
-    {
-        analogGlitch._isGlitch = true;
-
-        yield return new WaitForSecondsRealtime(0.75f);
-
-        analogGlitch._isGlitch = false;
-
-        yield return new WaitForSecondsRealtime(1.25f);
-
-        bossHand.holdOutHand();
-    }
->>>>>>> seonghui-develop:Assets/Script/HangingManager.cs
 }
