b16dbf33c6b2e54978e38aac8ce41bb509a998a7
eovywjr1 <40791869+eovywjr1@users.noreply.github.com>
2023-04-16 12:44:14 +0900

    민수 머지

--- Assets/Script/EventManager.cs
+++ Assets/Script/EventManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public interface IListener
 {
@@ -69,7 +70,7 @@ public class EventManager : MonoBehaviour
         listeners = newListeners;
     }
 
-    private void OnLevelWasLoaded()
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         RemoveRedundancies();
     }
