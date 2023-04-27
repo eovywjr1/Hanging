cdf53b01dd838cf5ed5143cf090e3061737a67f8
Umin73 <trillionumin@gmail.com>
2023-04-27 13:14:27 +0900

    230427_SolveEerror_ver2

--- Assets/Script/UiManager.cs
+++ Assets/Script/UiManager.cs
@@ -18,9 +18,17 @@ public class UiManager : MonoBehaviour
 
     private void guideOn()
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            GuideButton.SetActive(true);
+            if (GuideButton.activeSelf == false)
+            {
+                GuideButton.SetActive(true);
+
+            }
+            else
+            {
+                GuideButton.SetActive(false);
+            }
         }
     }
 }
