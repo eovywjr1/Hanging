cdb529cf9688691a265fa94b50153496f693f930
Umin73 <trillionumin@gmail.com>
2023-06-02 14:47:03 +0900

    퇴근길_Peter걷기 애니메이션 추가

--- Assets/Script/StreetSceneScript/CharacterMovement.cs
+++ Assets/Script/StreetSceneScript/CharacterMovement.cs
@@ -5,33 +5,30 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] float movePower = 3f;
-    [SerializeField] float jumpPower = 3f;
 
-    SpriteRenderer characterRenderer;
-    Rigidbody2D rigid;
-    Vector3 vector;
-
-    bool isJumping = false;
-    bool isRunning = false;
+    Transform characterTransform;
+    Animator animator;
 
     private void Start()
     {
-        rigid = gameObject.GetComponent<Rigidbody2D>();
-        characterRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        if(Input.GetButtonDown("Jump"))
-        {
-            isJumping = true;
-        }
+        characterTransform = gameObject.GetComponent<Transform>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        if(Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Horizontal") > 0)
+        {
+            animator.SetBool("isWalk", true);
+            animator.SetBool("isIdle", false);
+        }
+        else
+        {
+            animator.SetBool("isWalk", false);
+            animator.SetBool("isIdle", true);
+        }
+
         Move();
-        Jump();
     }
 
     void Move()
@@ -42,27 +39,15 @@ public class CharacterMovement : MonoBehaviour
         {
             moveVelocity = Vector3.left;
 
-            characterRenderer.flipX = true;
+            characterTransform.localScale = new Vector3(-1, 1, 1);
         }
         else if(Input.GetAxisRaw("Horizontal") > 0)
         {
             moveVelocity = Vector3.right;
 
-            characterRenderer.flipX = false;
+            characterTransform.localScale = new Vector3(1, 1, 1);
         }
 
         transform.position += moveVelocity * movePower * Time.deltaTime;
-    }
-
-    void Jump()
-    {
-        if (!isJumping) return;
-
-        rigid.velocity = Vector2.zero;
-
-        Vector2 jumpVelocity = new Vector2(0, jumpPower);
-        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
-
-        isJumping = false;
     }
 }
