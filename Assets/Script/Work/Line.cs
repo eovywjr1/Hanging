2fff3f483db52511a33b929bbe6da02e682801de
Umin73 <trillionumin@gmail.com>
2023-06-04 00:39:55 +0900

    사형수 정보 생성 속도 수정

--- Assets/Script/Work/Line.cs
+++ Assets/Script/Work/Line.cs
@@ -94,7 +94,7 @@ public class Line
     public IEnumerator ExpendLine(float _parentX, float _parentYSum)
     {
         int i = 1;
-        const float devide = 150f; //�и𿩼� �ӵ��� �ݺ�ʰ���
+        const float devide = 25f; //�и𿩼� �ӵ��� �ݺ�ʰ���
         float d = i / devide;
         while (d < 1)
         {
