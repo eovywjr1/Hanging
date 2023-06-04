0f4b12d839d6a8ac110df9af6e2a253c7cc5e1b4
JoMinsu <eovywjr1@naver.com>
2023-06-04 18:47:14 +0900

    1일차로 복구

--- Assets/Script/Work/HangingManager.cs
+++ Assets/Script/Work/HangingManager.cs
@@ -36,7 +36,7 @@ public class HangingManager : MonoBehaviour, IListener
     private int _discorrectJudgeCount = 0;
     private int _discorrectAndTodesstrafedPersonCount = 0;
 
-    public static int day = 2;
+    public static int day = 1;
 
     private void Awake()
     {
