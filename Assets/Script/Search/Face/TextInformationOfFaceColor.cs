d63348e999a02a8cc7b08f17140a5c1fb9109d55
Umin73 <trillionumin@gmail.com>
2023-07-21 16:03:38 +0900

    얼굴수색_ver1
    얼굴수색 아트 적용 전 버전

---
+++ Assets/Script/Search/Face/TextInformationOfFaceColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextInformationOfFaceColor : MonoBehaviour
+{
+    [SerializeField] TMP_Text information;
+    [SerializeField] FaceColorData faceColorData;
+
+    void Start()
+    {
+        information.text = "머리카락 : " + faceColorData.getHairColorName() + "\n";
+        information.text += "눈동자 : " + faceColorData.getEyeColorName();
+    }
+}
